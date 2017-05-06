6f6b83b345f40cea53935828b82de33c05b6b10a
bluescv <bluescv@gmail.com>
2017-05-06 14:39:02 +0800

    file and json test

--- freetest/jsontest.py
+++ freetest/jsontest.py
@@ -13,4 +13,17 @@ for key, value in errorcode.items():
         print(value)
 # print(repr(errorcode))
 
-print('end')
+print('---end---')
+
+'''
+codes used to generate json object then dump to json string
+'''
+result_dict = {}
+result_dict['name'] = 'name1'
+result_dict['gender'] = 1
+
+for key, value in result_dict.items():
+    print(key, value)
+
+result_json = json.dumps(result_dict)
+print(result_json)
---
+++ iotest/file.py
@@ -0,0 +1,5 @@
+str = 'abc'
+
+
+with open('/data/elk/userdata/user.txt', 'a') as f:
+    f.write("test12")
